f4a92b60b91eea3a2df0288fef22ad2622309c32
JauSingSing <chaunceyq@qq.com>
2023-08-03 09:35:57 +0800

    ci: 添加本地 ci 操作（husky/lint-staged/commintlit）

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting',
